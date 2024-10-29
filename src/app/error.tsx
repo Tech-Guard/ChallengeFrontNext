6a9183d5af958b3300c41bfb35962ec20fd913e2
JulioDev01 <juliocesarmaciel21@gmail.com>
2024-10-29 10:56:28 -0300

    Adicionando o loading para melhor fluidez

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,25 @@
 "use client"; 
+import styled from 'styled-components';
 
-export default function Error(){
-    return(
-        <h1>Erro 404 - Página não encontrada</h1>
-    )
+const ErrorContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f8f8f8;
+`;
+
+const ErrorMessage = styled.h1`
+  font-size: 2.5rem;
+  color: #d9534f;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export default function Error() {
+  return (
+    <ErrorContainer>
+      <ErrorMessage>Erro 404 - Página não encontrada</ErrorMessage>
+    </ErrorContainer>
+  );
 }
---
+++ src/app/loading.tsx
@@ -0,0 +1,35 @@
+"use client";
+import styled, { keyframes } from 'styled-components';
+
+const spinAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const SpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const SpinnerCircle = styled.div`
+  border: 8px solid rgba(0, 0, 0, 0.1);
+  border-top: 8px solid #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: ${spinAnimation} 1s linear infinite;
+`;
+
+export default function Spinner() {
+  return (
+    <SpinnerContainer>
+      <SpinnerCircle />
+    </SpinnerContainer>
+  );
+}
